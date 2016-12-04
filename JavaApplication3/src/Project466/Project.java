e93fbf62160e696dc9f0ac507d4a9cab840ad339
Nathan Sylvia <nathansylvia@pcp130331pcs.wireless.calpoly.edu>
2016-12-04 15:59:38 -0800

    bagging

--- JavaApplication3/src/Project466/Project.java
+++ JavaApplication3/src/Project466/Project.java
@@ -9,11 +9,14 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Random;
+import java.util.Set;
+import java.util.Iterator;
 
 public class Project {
 
 	static boolean debug = true;
-        static final int k = 10;
+        static final int bagSize = 10;
 	static ArrayList<IncomePerson> data = new ArrayList<>();
 	static DataParser dp = new DataParser();
 	static InputManager im = new InputManager();
@@ -27,13 +30,14 @@ public class Project {
 		System.out.println("Filter: "+filter);
 		data = filterData(data, filter);
 		
-		List<IncomePerson> nearestNeighbors = findKNN(inputPerson, 50,kNNRatio );
+		List<IncomePerson> nearestNeighbors = findKNN(data, inputPerson, 50,kNNRatio);
 
 		for (IncomePerson ip : nearestNeighbors) {
 			System.out.println(ip.toString());
 		}
-
+                //printBaggingResult(bagging(data, "income", .33));
 		System.out.println("Most probable category: "+ mostProbableCategory("income", nearestNeighbors));
+                printBaggingResult(bagging(data, "income", .33));
 	}
 
 	public static ArrayList<IncomePerson> filterData(ArrayList<IncomePerson> al, IncomePerson filter) {
@@ -61,24 +65,70 @@ public class Project {
 		return res;
 	}
 
-	public static List<IncomePerson> findKNN(IncomePerson key, int k, int kNNRatio) {
-
+	public static List<IncomePerson> findKNN(ArrayList<IncomePerson> dataSet, 
+         IncomePerson key, int k, double kNNRatio) {
+                System.out.println("kNNfunction");
 		ArrayList<IncomePerson> ipsWithDist = new ArrayList<>();
-		for (IncomePerson ip : data) {
+		for (IncomePerson ip : dataSet) {
 			ip.getDistance(key);
 			ipsWithDist.add(ip);
 		}
 
 		Collections.sort(ipsWithDist);
-
-		return ipsWithDist.subList(0, kNNRatio*ipsWithDist.size());
+                //System.out.println("kNN");
+		return ipsWithDist.subList(0, k);
 
 	}
         
-        public static String bagging(ArrayList<IncomePerson> data, String category) {
+        public static ArrayList<String> bagging(ArrayList<IncomePerson> data, 
+         String category, double ratio) {
         
+            ArrayList<IncomePerson> dataSet = new ArrayList<IncomePerson>();
+            ArrayList<String> result = new ArrayList<String>();
+            Random randomGenerator = new Random();
             
+            for(int i = 0; i < bagSize; i++) {
+                System.out.println("bag");
+                for(int j = 0; j < (int)(ratio * data.size()); j++) {
+                    System.out.println("here");
+                    dataSet.add(data.get(randomGenerator.nextInt(data.size())));
+                }
+                System.out.println("kNN");
+                List<IncomePerson> kNN = findKNN(dataSet, im.getInputPerson(), 50, 0.25);
+                result.add(mostProbableCategory(category, kNN));
+                dataSet.clear();
+                System.out.println("this");
+            }
+            //System.out.println(result);
+            return result;
+        }
+        
+        public static void printBaggingResult(ArrayList<String> result) {
             
+           HashMap<String, Integer> filteredResults = new HashMap<String, Integer>();
+           Set<String> keys;
+           Iterator it;
+           String majorityFilter = "";
+           int majority = 0;
+           for(int i = 0; i < result.size(); i++) {
+               if(!(filteredResults.containsKey(result.get(i)))) {
+                   filteredResults.put(result.get(i), 1);
+               }
+               else {
+                   filteredResults.put(result.get(i), filteredResults.get(result.get(i)) + 1);
+               }
+           }
+           keys = filteredResults.keySet();
+           it = keys.iterator();
+           while(it.hasNext()) {
+               String current = ((String)it.next());
+               if(filteredResults.get(current) > majority) {
+                   majority = filteredResults.get(current);
+                   majorityFilter = current;
+               }
+           }
+           System.out.println("Bagging Result: Majority Class: " + majorityFilter 
+            + " Accuracy: " + majority + "/" + result.size());
         }
 
 	public static String mostProbableCategory(String filter, List<IncomePerson> listNN) {
