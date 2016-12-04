caa88eed4b296685c84befda2b4276301f0a0156
Gideon <kc.gideonw@gmail.com>
2016-12-04 13:01:47 -0800

    debugging filtering

--- JavaApplication3/src/Project466/Project.java
+++ JavaApplication3/src/Project466/Project.java
@@ -9,62 +9,64 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Project {
+	static boolean debug = true;
+
 	static ArrayList<IncomePerson> data = new ArrayList<>();
 	static ArrayList<IncomePerson> filteredData;
 	static DataParser dp = new DataParser();
 	static InputManager im = new InputManager();
-	
-	public static void main(String args[]){
+
+	public static void main(String args[]) {
 		data = dp.parseData();
 		IncomePerson inputPerson = im.getInputPerson();
 		IncomePerson filter = im.getFilter();
-		System.out.println(getDistance(data.get(0), data.get(1)));
-                
-                List<IncomePerson> nearestNeighbors = findKNN(inputPerson, 50);
-                
-                for(IncomePerson ip : nearestNeighbors) {
-                    System.out.println(ip.toString());
-                }
+		
 		filteredData = filterData(data, filter);
+
+		List<IncomePerson> nearestNeighbors = findKNN(inputPerson, 50);
+
+		for (IncomePerson ip : nearestNeighbors) {
+			System.out.println(ip.toString());
+		}
 	}
-	
-	public static ArrayList<IncomePerson> filterData(ArrayList<IncomePerson> al, IncomePerson filter){
+
+	public static ArrayList<IncomePerson> filterData(ArrayList<IncomePerson> al, IncomePerson filter) {
 		ArrayList<IncomePerson> fal = new ArrayList<>();
-		for(int i=0; i<al.size(); i++){
-			if(filter.matches(al.get(i))){
+		for (int i = 0; i < al.size(); i++) {
+			if(al.get(i).equals("Black")){
+			System.out.println("!");
+		}
+			if (filter.matches(al.get(i))) {
 				fal.add(al.get(i));
 			}
 		}
-		System.out.println("Filter run. "+(al.size()-fal.size())+" tuples removed.");
+		System.out.println("Filter run. " + (al.size() - fal.size()) + " tuples removed.");
 		return fal;
 	}
-	
+
 	/* get 3d distance between 2 people */
-	public static double getDistance(IncomePerson a, IncomePerson b){
+	public static double getDistance(IncomePerson a, IncomePerson b) {
 		double ageWgt = 1, eduWgt = 1, hoursWgt = 1;
-		double res =
-				Math.sqrt(
-						ageWgt * Math.pow( (double)a.age - (double)b.age, 2 )
-						+ eduWgt * Math.pow( (double)a.edu - (double)b.edu, 2 )
-						+ hoursWgt * Math.pow( (double)a.hours - (double)b.hours, 2 )
+		double res
+				= Math.sqrt(
+						ageWgt * Math.pow((double) a.age - (double) b.age, 2)
+						+ eduWgt * Math.pow((double) a.edu - (double) b.edu, 2)
+						+ hoursWgt * Math.pow((double) a.hours - (double) b.hours, 2)
 				);
 		return res;
 	}
-        
-        
-        public static List<IncomePerson> findKNN(IncomePerson key, int k) {
-            
-            
-            ArrayList<IncomePerson> ipsWithDist= new ArrayList<>();
-            for(IncomePerson ip : data) {
-                ip.getDistance(key);
-                ipsWithDist.add(ip);
-            }
-            
-            Collections.sort(ipsWithDist);
-            
-            return  ipsWithDist.subList(0, k);
-            
 
-        }
+	public static List<IncomePerson> findKNN(IncomePerson key, int k) {
+
+		ArrayList<IncomePerson> ipsWithDist = new ArrayList<>();
+		for (IncomePerson ip : data) {
+			ip.getDistance(key);
+			ipsWithDist.add(ip);
+		}
+
+		Collections.sort(ipsWithDist);
+
+		return ipsWithDist.subList(0, k);
+
+	}
 }
